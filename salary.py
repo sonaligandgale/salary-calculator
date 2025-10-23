526b4712242b37ff097fc4de953ff03e24055ea8
sonaligandgale <sonaligandgalekar@gmail.com>
2025-10-23 16:56:12 +0530

    Create salary.py

---
+++ salary.py
@@ -0,0 +1,6 @@
+# Salary + Bonus Calculator
+salary = float(input("Enter employee salary: ₹"))
+print("Original Salary: ₹", salary)
+
+bonus_salary = salary * 1.10
+print("Bonus Added: ₹", bonus_salary)
